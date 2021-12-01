fd4065d5b48bfe1920872f312ccbbbfeffd69dad
onePotMeatSpk <ysj1263743543@gmail.com>
2021-12-01 16:19:03 +0800

    添加： 二叉搜索树迭代器.h 树的子结构.h

---
+++ 二叉搜索树迭代器.h
@@ -0,0 +1,61 @@
+#pragma once
+#include<iostream>
+#include<stack>
+#include"二叉树.h"
+using namespace std;
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        nodeRoot = root;
+    }
+
+    int next() {
+        //迭代器还是初始状态
+        if (s.empty() && nodeRoot)
+        {
+            TreeNode* temp = nodeRoot;
+            nodeRoot = NULL;
+            while (temp)
+            {
+                s.push(temp);
+                temp = temp->left;
+            }
+        }
+        //迭代器已非初始状态
+        //当前节点没有右子树，则next为其父节点
+        else if (s.top()->right == NULL)
+            s.pop();
+        //当前节点有右子树，则next为其右子树的最左侧节点
+        else
+        {
+            TreeNode* temp = s.top()->right;
+            s.pop();
+            s.push(temp);
+            temp = temp->left;
+            while (temp)
+            {
+                s.push(temp);
+                temp = temp->left;
+            }
+        }
+
+        return s.top()->val;
+    }
+
+    bool hasNext() {
+        //若当前迭代器还是初始状态
+        if (nodeRoot)    return true;
+        //若当前节点有右子树
+        if (s.top()->right)  return true;
+        //若当前节点有父节点
+        if (s.size() >= 2) return true;
+
+        return false;
+
+    }
+
+private:
+    stack<TreeNode*> s;
+    TreeNode* nodeRoot;
+};
---
+++ 树的子结构.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include"二叉树.h"
+using namespace std;
+
+bool isTheSame(TreeNode* A, TreeNode* B);
+
+//输入两个树，判断B是否A之子结构，约定空树非任意树的子结构
+bool isSubStructure(TreeNode* A, TreeNode* B) {
+	//B为空树，非任意树子结构
+	if (!B)	return false;
+	//A为空，说明已经到达A树叶子也没找到B
+	if (!A)	return false;
+	//AB相同，则判断AB子结构是否相同
+	if (A->val == B->val)
+		if (isTheSame(A, B))
+			return true;
+	//AB不同或者AB子结构不同，均向下递归左右子树
+	return isSubStructure(A->left, B) || isSubStructure(A->right, B);
+}
+
+bool isTheSame(TreeNode* A, TreeNode* B) {
+	//如果B空，说明已经将B遍历完毕
+	if (!B)	return true;
+	//如果B不空但是A空，则A不可能与B同
+	if (!A)	return false;
+	//如果两者值不一样
+	if (A->val != B->val)	return false;
+	//如果两者左子树或者右子树不一样
+	if ((!isTheSame(A->left, B->left)) || (!isTheSame(A->right, B->right)))	return false;
+	//以上都一样则说明两者完全相同
+	return true;
+}
