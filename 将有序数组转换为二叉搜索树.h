e46b1696b5d0f1e21ddd3610cf8c2a1107c8b2f4
onePotMeatSpk <ysj1263743543@gmail.com>
2022-05-30 11:48:21 +0800

    修改 将有序数组转换为二叉搜索树

--- 将有序数组转换为二叉搜索树.h
+++ 将有序数组转换为二叉搜索树.h
@@ -22,3 +22,47 @@ TreeNode* dfsSortedArrayToBST(vector<int>& nums, int l, int r) {
 TreeNode* sortedArrayToBST(vector<int>& nums) {
     return dfsSortedArrayToBST(nums, 0, nums.size() - 1);
 }
+
+
+//·½·¨2£º²ãÐòµü´ú
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    queue<TreeNode*> q;
+    queue<int> qL;
+    queue<int> qR;
+
+    TreeNode* root = new TreeNode(0);
+    q.push(root);
+    qL.push(0);
+    qR.push(nums.size() - 1);
+
+    while (!q.empty())
+    {
+        TreeNode* temp = q.front();
+        q.pop();
+        int l = qL.front();
+        int r = qR.front();
+        qL.pop();
+        qR.pop();
+
+        int m = l + (r - l) / 2;
+        temp->val = nums[m];
+
+        if (l <= m - 1)
+        {
+            temp->left = new TreeNode(0);
+            q.push(temp->left);
+            qL.push(l);
+            qR.push(m - 1);
+        }
+
+        if (r >= m + 1)
+        {
+            temp->right = new TreeNode(0);
+            q.push(temp->right);
+            qL.push(m + 1);
+            qR.push(r);
+        }
+    }
+
+    return root;
+}
