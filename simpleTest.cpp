2aa285c70b4edbf1a86b216cc18ed0c2c1acda5e
onePotMeatSpk <ysj1263743543@gmail.com>
2022-06-07 17:53:54 +0800

    修改 数据流中的中位数

--- simpleTest.cpp
+++ simpleTest.cpp
@@ -1,62 +1,14 @@
 #pragma once
 #include<iostream>
 #include<string>
-
-#include<queue>
-#include"数据流中的中位数.h"
+#include<vector>
 using namespace std;
 
-struct S
-{
-	int i;
-	S(int _i) :i(_i)
-	{
 
-	}
-
-	bool operator<(const S& s1)	const
-	{
-		return i < s1.i;
-	}
-
-	bool operator>(const S& s1)	const
-	{
-		return i > s1.i;
-	}
-
-	bool operator==(const S& s1)	const
-	{
-		return i == s1.i;
-	}
-};
 
 
 void main()
 {
-	priority_queue<S, vector<S>, less<S>> p1;
-
-	priority_queue<S, vector<S>, greater<S>> p2;
-
-	for (int i = 0; i < 5; i++)
-	{
-		p1.push(S(i));
-		p2.push(S(i));
-	}
-
-
-	for (int i = 0; i < 5; i++)
-	{
-		cout << p1.top().i << " ";
-		p1.pop();
-	}
-	cout << endl;
-
-	for (int i = 0; i < 5; i++)
-	{
-		cout << p2.top().i << " ";
-		p2.pop();
-	}
-	cout << endl;
 
 
 	return;
--- 数据流中的中位数.h
+++ 数据流中的中位数.h
@@ -18,7 +18,7 @@ using namespace std;
 
 //思路：使用两个堆，一个大顶堆、一个小顶堆，大顶堆存放较小一半元素，小顶堆存放较大一半元素，两者的顶即为整体的中间的数字
 
-//方法1：使用priority_queue
+//实现方法1：使用priority_queue
 class MedianFinder {
 public:
 
@@ -65,4 +65,118 @@ public:
 };
 
 
-//方法2：手撕堆
+//实现方法2：手撕堆
+class MyHeap
+{
+private:
+	vector<int> nums;
+	bool flagMaxMin;
+
+public:
+	MyHeap(bool flag) :flagMaxMin(flag) {}
+
+	void push(int num)
+	{
+		nums.push_back(num);
+		pushHeap(nums.size() - 1);
+	}
+
+	void pop()
+	{
+		popHeap(nums.size());
+		nums.pop_back();
+	}
+
+	int top()
+	{
+		if (nums.empty())	return INT_MIN;
+		return nums[0];
+	}
+
+	int size()
+	{
+		return nums.size();
+	}
+
+private:
+	void pushHeap(int hole)
+	{
+		int val2Push = nums[hole];
+		int parent = (hole - 1) / 2;
+
+		while (hole > 0 && (flagMaxMin ? val2Push > nums[parent] : val2Push < nums[parent]))
+		{
+			nums[hole] = nums[parent];
+			hole = parent;
+			parent = (hole - 1) / 2;
+		}
+
+		nums[hole] = val2Push;
+	}
+
+	void popHeap(int len)
+	{
+		int hole = 0;
+		int val2Pop = nums[hole];
+		int secondChild = hole * 2 + 2;
+
+		while (secondChild < len)
+		{
+			if (flagMaxMin ? nums[secondChild - 1] > nums[secondChild] : nums[secondChild - 1] < nums[secondChild])	secondChild--;
+
+			nums[hole] = nums[secondChild];
+			hole = secondChild;
+			secondChild = hole * 2 + 2;
+		}
+
+		if (secondChild == len)
+		{
+			secondChild--;
+			nums[hole] = nums[secondChild];
+			hole = secondChild;
+		}
+
+		swap(nums[hole], nums[len - 1]);
+		nums[len - 1] = val2Pop;
+
+		if (hole != len - 1)	pushHeap(hole);
+	}
+};
+
+
+class MedianFinder {
+public:
+
+    MedianFinder() :maxHeap(1), minHeap(0) {}
+
+    void addNum(int num)
+    {
+        if (maxHeap.size() == minHeap.size())
+        {
+            minHeap.push(num);
+            int temp = minHeap.top();
+            minHeap.pop();
+            maxHeap.push(temp);
+        }
+        else
+        {
+            maxHeap.push(num);
+            int temp = maxHeap.top();
+            maxHeap.pop();
+            minHeap.push(temp);
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() == 0 && minHeap.size() == 0)  return 0;
+
+        if (maxHeap.size() == minHeap.size())    return ((double)maxHeap.top() + (double)minHeap.top()) / 2.0;
+        else    return maxHeap.top();
+    }
+
+public:
+    // 最大堆，存储左边一半的数据，堆顶为最大值
+    MyHeap maxHeap;
+    // 最小堆， 存储右边一半的数据，堆顶为最小值
+    MyHeap minHeap;
+};
